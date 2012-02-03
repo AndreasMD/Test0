15bd4c5f5348c0c3c6f4ad5ffc0ff96bcff51d87
Andreas M. Daum <Daum@mfapp.de>
2012-02-03 11:55:18 +0100

    Initial Commit

---
+++ Test0/MFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MFAppDelegate.h
+//  Test0
+//
+//  Created by Andreas Daum on 03.02.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
